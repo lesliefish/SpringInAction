1c2a5ddecf6b6137a4d8ccaa1879d6708a4d2fd3
leiyu <yuleics@outlook.com>
2018-12-09 23:10:34 +0800

    hello world  example pass

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+import helloworld.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+        helloWorld.getMessage();
+    }
+}
---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class HelloWorld {
+    private String message;
+
+    public void getMessage() {
+        System.out.println("Message is " + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
