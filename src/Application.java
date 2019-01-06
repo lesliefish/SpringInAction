81bd80161683a7b0edf75575b87e79db81103af9
leiyu <yuleics@outlook.com>
2019-01-06 23:32:10 +0800

    依赖注入  基于构造函数

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,7 @@ import beaninheritance.BeanInheritance1;
 import beaninheritance.BeanInheritance2;
 import beanlifecycle.BeanLifeCycleTest;
 import beanpostprocessor.BeanPostProcessorDemo;
+import dependencyinjection.ConstructorBased;
 import helloworld.HelloWorld;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,13 +15,7 @@ public class Application {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
-        BeanInheritance1 objA = (BeanInheritance1) context.getBean("helloWorld");
-        objA.getMessage1();
-        objA.getMessage2();
-
-        BeanInheritance2 objB = (BeanInheritance2) context.getBean("helloIndia");
-        objB.getMessage1();
-        objB.getMessage2();
-        objB.getMessage3();
+        ConstructorBased constructorBased = (ConstructorBased)context.getBean("constructorBased");
+        constructorBased.saySth();
     }
 }
---
+++ src/dependencyinjection/ConstructorBased.java
@@ -0,0 +1,13 @@
+package dependencyinjection;
+
+public class ConstructorBased {
+    private Speaker speaker;
+    public ConstructorBased(Speaker s){
+        System.out.println("ConstructorBased Construct");
+        speaker = s;
+    }
+
+    public void saySth(){
+        speaker.speak();
+    }
+}
