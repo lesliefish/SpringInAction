5a071a183d843d6ed2d5b580d0b3554238aa2d66
lesliefish <yuleics@outlook.com>
2019-07-14 12:12:55 +0800

    基于构造方法的自动装配

---
+++ AutowiringByConstructor/src/com/lesliefish/MainApp.java
@@ -0,0 +1,11 @@
+package com.lesliefish;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        context.getBean(TextEditor.class).spellCheck();
+    }
+}
